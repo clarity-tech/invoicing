eec372c96b83b0d67ab5cef88c2a7af855ab93ea
mohitp <mohit@claritytech.io>
2025-12-05 05:10:39 +0530

    fix: register and update team issue

--- resources/views/teams/show.blade.php
+++ resources/views/teams/show.blade.php
@@ -7,7 +7,7 @@
 
     <div>
         <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
-            @livewire('teams.update-team-name-form', ['team' => $team])
+            @livewire(\App\Livewire\Teams\UpdateTeamNameForm::class, ['team' => $team])
 
             @livewire('teams.team-member-manager', ['team' => $team])
 
