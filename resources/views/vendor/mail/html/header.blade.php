3bd541264623f4dfc442c223cee1af3b66b55de4
Manash Sonowal <manash149@gmail.com>
2026-05-03 22:56:26 +0530

    feat(mail): add brand mark logo to email header
    Render the InvoiceInk ink-drop mark alongside the styled wordmark
    in transactional email headers (reset password, verify email, team
    invitation). Uses an inline table layout for Outlook compatibility.
    
    Co-Authored-By: jupitora <84776679+boruah@users.noreply.github.com>

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,11 +1,20 @@
 @props(['url'])
 <tr>
 <td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
+<a href="{{ $url }}" style="display: inline-block; text-decoration: none;">
 @if (trim($slot) === 'Laravel')
 <img src="https://laravel.com/img/notification-logo-v2.1.png" class="logo" alt="Laravel Logo">
 @else
-{!! $slot !!}
+<table cellpadding="0" cellspacing="0" border="0" role="presentation" style="margin: 0 auto; border-collapse: collapse;">
+<tr>
+<td style="vertical-align: middle; padding-right: 10px;">
+<img src="{{ asset('images/email-logo-mark.png') }}" alt="" width="36" height="36" style="display: block; border: 0;">
+</td>
+<td style="vertical-align: middle;">
+<span class="logo-text">{!! $slot !!}</span>
+</td>
+</tr>
+</table>
 @endif
 </a>
 </td>
